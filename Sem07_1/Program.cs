7ae3a2acbfbc36a4bf9e255c821979188bebb1db
Elvis Mosqueira <n00447800@upn.pe>
2025-05-08 11:45:26 -0500

    Sem01_1

--- Sem07_1/Program.cs
+++ Sem07_1/Program.cs
@@ -14,7 +14,42 @@ namespace Sem07_1
             int alea, bing;
             Random R = new Random(); 
             alea = R.Next(1,10);
+            Console.WriteLine("Numero aleatorio generado " + alea);
+            do
+            {
 
+                Console.WriteLine("Ingrese un numero");
+                bing = int.Parse(Console.ReadLine());
+                if (bing == alea)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("¡Adivinaste!");
+                    Console.Beep(800, 400);
+                    Console.Beep(8001,400);
+                    Console.Beep(1300, 500);
+                    Console.Beep(8001, 1000);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                else
+                {//numero ingresado =5      numero aleatorio=6
+                    if(bing>alea)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine("Ingrese un n° mayor al anterior");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.BackgroundColor = ConsoleColor.Black;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Ingrese un n° menor al anterior");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.BackgroundColor = ConsoleColor.Black;
+                    }
+                }
+            }
+            while (bing != alea); //True, no adivinaste
         }
     }
 }
