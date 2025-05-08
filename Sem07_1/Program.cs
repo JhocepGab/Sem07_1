1b8ff52e4b7e7f6a52b0896233855c581183b4c5
Elvis Mosqueira <n00447800@upn.pe>
2025-05-08 11:16:05 -0500

    Agregar archivos de proyecto.

---
+++ Sem07_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem07_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //juego sobre adivinar que numero, emitir sonidos 
+            int alea, bing;
+            Random R = new Random(); 
+            alea = R.Next(1,10);
+
+        }
+    }
+}
